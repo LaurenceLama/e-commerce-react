54adf5cfa82baf7ffbe6d714436c5706bde01e43
LaurenceLama <laurencelamaFSE@gmail.com>
2023-04-26 19:01:57 +0800

    Initializing docs folder for website publication

---
+++ docs/src/components/Featured.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import BestBooks from "./ui/BestBooks";
+
+const Featured = () => {
+  return (
+    <section id="features">
+      <div className="container">
+        <div className="row">
+          <h2 className="section__title">
+            Featured <span className="purple">Books</span>
+          </h2>
+          <BestBooks />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Featured;
